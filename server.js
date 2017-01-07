72302bc9fc2bb228a47e54cf4e65e6656a2f6bbf
Zuber <zuber00124@gmail.com>
2017-01-07 16:28:16 +0530

    serverjs change

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
 
-//create our app
+// Create our app
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next) {
-  if (req.header['x-forwarded-proto'] === 'http'){
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'http') {
     next();
-  }else {
-    req.redirect('http://' + req.hostname + req.url);
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
 app.use(express.static('public'));
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log('Express server is up on port ' + PORT);
 });
