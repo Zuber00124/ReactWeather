1fcb7aaee531082daf82d39e88b62ca280236fdb
Zuber <zuber00124@gmail.com>
2017-01-11 18:12:02 +0530

    Last

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -14,33 +14,29 @@ var ErrorModal = React.createClass({
   },
   componentDidMount: function () {
     var {title, message} = this.props;
+    var modalMarkup = (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p>
+          <button className="button hollow" data-close="">
+            Okay
+          </button>
+        </p>
+      </div>
+    );
 
-    var modalmarkup = (
-          <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-            <h4>{title}</h4>
-            <p>{message}</p>
-            <p>
-              <button className="button hollow" data-close="">
-                Okay
-              </button>
-            </p>
-          </div>
-        );
-
-      var $modal= $(ReactDOMServer.renderToString(modalmarkup));
-      $(ReactDOM.findDOMNode(this)).html($modal);
+    var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
 
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
   },
   render: function () {
-
-    return(
+    return (
       <div>
-
       </div>
     );
-
   }
 });
 
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -16,7 +16,7 @@ var Weather = React.createClass({
     this.setState({
       isLoading: true,
       errorMessage: undefined,
-      location : undefined,
+      location: undefined,
       temp: undefined
     });
 
@@ -33,18 +33,18 @@ var Weather = React.createClass({
       });
     });
   },
-  componentDidMount: function (){
+  componentDidMount: function () {
     var location = this.props.location.query.location;
 
-    if (location && location.length > 0){
+    if (location && location.length > 0) {
       this.handleSearch(location);
       window.location.hash = '#/';
     }
   },
-  componentWillReceiveProps: function (newProps){
+  componentWillReceiveProps: function (newProps) {
     var location = newProps.location.query.location;
 
-    if (location && location.length > 0){
+    if (location && location.length > 0) {
       this.handleSearch(location);
       window.location.hash = '#/';
     }
